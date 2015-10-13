4787b6a651fa71d3234b6c5888ec3bbb7f03d549
Dominik Zogg <dominik.zogg@gmail.com>
2015-10-13 20:55:31 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# create-symfony-project
+
+## installation
+
+### clone
+
+```{.sh}
+cd ~
+git clone https://github.com/vagrant-php/create-symfony-project.git
+```
+
+### register path
+
+add the following line to your `.bashrc` or `.bash_profile` depending on your system
+
+```{.sh}
+export PATH=~/create-symfony-project/bin:$PATH
+```
+
+## update
+
+### pull
+```{.sh}
+cd ~/create-symfony-project
+git pull
+```
+
+## usage
+
+```{.sh}
+create-symfony-project --help
+```
+
+## credentials
+
+### installation password
+
+kZOPhGztaTZFGng8z76Z
+
+### backend user
+
+admin // nMiuZ7890HtGfaszZGH
---
+++ files/3.5/localconfig.php
@@ -0,0 +1,19 @@
+<?php
+
+### INSTALL SCRIPT START ###
+$GLOBALS['TL_CONFIG']['licenseAccepted'] = true;
+$GLOBALS['TL_CONFIG']['installPassword'] = '$2y$10$engj.oelE56rdeQfb2WF1eXw7catAaH92t7ypZDmcI0Ll2lDP.2cK';
+$GLOBALS['TL_CONFIG']['encryptionKey'] = 'fe7f52ef33a014fc4626e79e1c4c9214';
+$GLOBALS['TL_CONFIG']['dbDriver'] = 'MySQLi';
+$GLOBALS['TL_CONFIG']['dbHost'] = 'localhost';
+$GLOBALS['TL_CONFIG']['dbUser'] = 'root';
+$GLOBALS['TL_CONFIG']['dbPass'] = 'root';
+$GLOBALS['TL_CONFIG']['dbDatabase'] = 'contao';
+$GLOBALS['TL_CONFIG']['dbPconnect'] = false;
+$GLOBALS['TL_CONFIG']['dbCharset'] = 'UTF8';
+$GLOBALS['TL_CONFIG']['dbPort'] = 3306;
+$GLOBALS['TL_CONFIG']['dbSocket'] = '/var/run/mysqld/mysqld.sock';
+$GLOBALS['TL_CONFIG']['adminEmail'] = 'admin@admin.admin';
+$GLOBALS['TL_CONFIG']['latestVersion'] = '3.5.4';
+$GLOBALS['TL_CONFIG']['maintenanceMode'] = false;
+### INSTALL SCRIPT STOP ###
