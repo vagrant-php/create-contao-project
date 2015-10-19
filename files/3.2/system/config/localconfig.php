89349957bb15bcff04970764b07e3ab4ddb0edcb
Dominik Zogg <dominik.zogg@gmail.com>
2015-10-19 21:05:38 +0200

    add files for contao 3.2

---
+++ files/3.2/system/config/localconfig.php
@@ -0,0 +1,18 @@
+<?php
+
+### INSTALL SCRIPT START ###
+$GLOBALS['TL_CONFIG']['licenseAccepted'] = true;
+$GLOBALS['TL_CONFIG']['installPassword'] = '$6$9a75d5a2182e94a8$HY/f0i.kfWhfP/4K1X4/5cBX6WKxbSnpNZ6U1Qtpl9fulwtIAEBMR4ZvxDcrjdsaSyIXnBBjjaPV2Ois8K6vH0';
+$GLOBALS['TL_CONFIG']['encryptionKey'] = '5e17338094315182a729cb5e56f25a71';
+$GLOBALS['TL_CONFIG']['dbDriver'] = 'MySQLi';
+$GLOBALS['TL_CONFIG']['dbHost'] = 'localhost';
+$GLOBALS['TL_CONFIG']['dbUser'] = 'root';
+$GLOBALS['TL_CONFIG']['dbPass'] = 'root';
+$GLOBALS['TL_CONFIG']['dbDatabase'] = '%%%%name%%%%';
+$GLOBALS['TL_CONFIG']['dbPconnect'] = false;
+$GLOBALS['TL_CONFIG']['dbCharset'] = 'UTF8';
+$GLOBALS['TL_CONFIG']['dbPort'] = 3306;
+$GLOBALS['TL_CONFIG']['dbSocket'] = '/var/run/mysqld/mysqld.sock';
+$GLOBALS['TL_CONFIG']['adminEmail'] = 'admin@admin.admin';
+$GLOBALS['TL_CONFIG']['latestVersion'] = '3.5.4';
+### INSTALL SCRIPT STOP ###
